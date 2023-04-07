240614bf861c68584e8a6e6ab4a120874d874158
Nicolas Doyen <nico-foot10@hotmail.com>
2023-04-07 22:22:37 +0200

    feat: button back done

--- src/routes/CountryDetails.tsx
+++ src/routes/CountryDetails.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import {getCountryByNumericCode} from '../helpers';
-import {Country} from '../types';
+import { useParams, useNavigate } from "react-router-dom";
+import { BsArrowLeft } from "react-icons/bs";
+import { getCountryByNumericCode } from '../helpers';
+import { Country } from '../types';
 import countries from '../data.json';
 
 export const CountryDetails = () => {
-    const {numericCode} = useParams();
+    const { numericCode } = useParams();
+    const navigate = useNavigate();
     const [countryDetails, setCountryDetails] = useState<Country | {}>({})
 
     useEffect(() => {
@@ -16,8 +18,25 @@ export const CountryDetails = () => {
 
     console.log(countryDetails)
     return (
-        <div>
-            {numericCode}
+        <div className="country-details-container">
+            {Object.keys(countryDetails).length > 0 && (
+                <>
+                    <div>
+                        <button className="back-btn" onClick={() => navigate(-1)}>
+                            <BsArrowLeft className={"back-btn-icon"} />
+                            <p>Back</p>
+                        </button>
+                    </div>
+                    <div className="country-details-content">
+                        <div className="flag-container">
+                            <img src={countryDetails.flags.png} alt="" />
+                        </div>
+                        <div className="details-container">
+
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
