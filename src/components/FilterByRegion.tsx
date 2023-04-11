bf316f468582f177f2a7f2afddf1aed83431028b
Nicolas Doyen <nico-foot10@hotmail.com>
2023-04-11 19:23:19 +0200

    feat: filter countries by region

--- src/components/FilterByRegion.tsx
+++ src/components/FilterByRegion.tsx
@@ -1,20 +1,34 @@
 import { useState, useMemo } from 'react';
 import {IoIosArrowDown} from "react-icons/io";
+import { FilterByRegiontType } from '../types';
 
-export const FilterByRegion = () => {
+type Props = {
+    handleFilterByRegion: (value: FilterByRegiontType) => void;
+    region: FilterByRegiontType;
+}
+
+export const FilterByRegion = ({handleFilterByRegion, region}: Props) => {
     const [showDropdown, setShowDropdown] = useState<boolean>(false);
-    const regions = useMemo(() => (['Africa', 'America', 'Asia', 'Europe', 'Oceania']), []);
+    const regions = useMemo<FilterByRegiontType[]>(() => (['All', 'Africa', 'America', 'Asia', 'Europe', 'Oceania']), []);
+
     return (
        <div className='filter-region-container'>
          <button className="filter-btn" onClick={() => setShowDropdown(!showDropdown)}>
-            <p>Filter by Region</p>
+            <p>Filter by Region: {region}</p>
             <IoIosArrowDown />
         </button>
         {showDropdown && (
             <div className='dropdown-container'>
                 <ul>
                     {regions.map((region) => (
-                        <li key={region}>{region}</li>
+                        <li key={region} 
+                            onClick={() => {
+                                handleFilterByRegion(region);
+                                setShowDropdown(!showDropdown);
+                            }}
+                        >
+                            {region}
+                        </li>
                     ))}
                 </ul>  
             </div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,26 +1,43 @@
 import { useState, useEffect } from "react";
-import { Country, Countries } from '../types';
+
 import { CountryCard } from "../components/CountryCard";
 import { SearchBar } from '../components/SearchBar';
-import countries from "../data.json";
-import '../styles/index.scss'
-import { filterByCharacter } from "../helpers";
 import { FilterByRegion } from "../components/FilterByRegion";
 
+import { filterByCharacter } from "../helpers";
+import { Countries, FilterByRegiontType } from '../types';
+
+import countriesData from "../data.json";
+
+import '../styles/index.scss'
+
 export const Home = () => {
+    const [countries, setCountries] = useState(countriesData)
     const [countryInput, setCountryInput] = useState<string>("");
     const [searchedCountries, setSearchCountries] = useState<Countries | []>(countries);
+    const [filterByRegion, setFilterByRegion] = useState<FilterByRegiontType>("All")
+
     const handleSetCountry = (value: string) => setCountryInput(value);
+    const handleFilterByRegion = (value: FilterByRegiontType) => setFilterByRegion(value);
+
+    useEffect(() => {
+        if (filterByRegion !== "All") {
+            const filteredCountries = countriesData.filter(({region}) => region === filterByRegion);
+            setCountries(filteredCountries);
+        } else {
+            setCountries(countriesData);
+        }
+    }, [filterByRegion]);
 
     useEffect(() => {
         setSearchCountries(filterByCharacter(countryInput, countries))
-    }, [countryInput])
+    }, [countryInput, countries])
 
     return (
         <div>
             <div className="search-header">
                  <SearchBar handleChange={handleSetCountry} value={countryInput} />
-                 <FilterByRegion />
+                 <FilterByRegion handleFilterByRegion={handleFilterByRegion} region={filterByRegion} />
             </div>
             <div className={"cards-container"}>
                 {searchedCountries.map(country => (
